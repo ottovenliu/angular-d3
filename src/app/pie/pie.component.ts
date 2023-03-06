0589bc75f93a94fbf1ddcd3970819ef5a8f73637
otto_liu <otto_liu@kunyoutech.com.tw>
2023-03-06 13:47:55 +0800

    fix: pieChart_init_RWD - otto_liu

--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -12,7 +12,7 @@ export class PieComponent implements OnInit, AfterViewInit {
   @Input()
   chartName: string = 'bar';
   @Input()
-  data:any= [];
+  data: any = [];
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     const target = event.target as Window
@@ -20,7 +20,7 @@ export class PieComponent implements OnInit, AfterViewInit {
     this.rwdSvgHeight = this.rwdSvgWidth;
     this.width = this.rwdSvgWidth
     this.height = this.rwdSvgHeight
-    this.radius = Math.min(this.width, this.height) / 2 ;
+    this.radius = Math.min(this.width, this.height) / 2;
     if (target.innerWidth > 310 && target.innerWidth < 400) {
       this.createSvg();
       this.drawChart(this.data);
@@ -33,9 +33,9 @@ export class PieComponent implements OnInit, AfterViewInit {
   private svg: any;
   private margin = 50;
   private width = 750 - this.margin * 2;
-  private height = 400 - this.margin * 2;
+  private height = 750 - this.margin * 2;
   // The radius of the pie chart is half the smallest side
-  private radius = Math.min(this.width, this.height) / 2 ;
+  private radius = Math.min(this.width, this.height) / 2;
   private colors: any;
   private createSvg(): void {
     if (d3.select(`figure#${this.chartName} svg`)) { d3.select(`figure#${this.chartName} svg`).remove() }
@@ -49,10 +49,10 @@ export class PieComponent implements OnInit, AfterViewInit {
   }
   private createColors(): void {
     this.colors = d3.scaleOrdinal()
-      .domain(this.data.map((d:any )=> d.Stars.toString()))
+      .domain(this.data.map((d: any) => d.Stars.toString()))
       .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
   }
-  private drawChart(data:any): void {
+  private drawChart(data: any): void {
     // Compute the position of each group on the pie:
     const pie = d3.pie<any>().value((d: any) => Number(d.Stars));
 
@@ -75,22 +75,22 @@ export class PieComponent implements OnInit, AfterViewInit {
       .innerRadius(100)
       .outerRadius(this.radius);
 
-        this.svg
-        .selectAll('pieces')
-        .data(pie(data))
-        .enter()
-        .append('text')
-        .text((d: any) => d.data.Framework)
-        .attr("class", "label")
-        .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
-        .style("text-anchor", "middle")
-        .style("font-size", 15);
-        if (window.innerWidth < 520){
-        this.svg
+    this.svg
+      .selectAll('pieces')
+      .data(pie(data))
+      .enter()
+      .append('text')
+      .text((d: any) => d.data.Framework)
+      .attr("class", "label")
+      .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
+      .style("text-anchor", "middle")
+      .style("font-size", 15);
+    if (window.innerWidth < 520) {
+      this.svg
         .selectAll('.label')
         .attr("transform", (d: any) => "translate(" + labelLocation.centroid(d) + ")")
         .style("font-size", '2vw');
-      }
+    }
   }
 
   ngOnInit(): void {
