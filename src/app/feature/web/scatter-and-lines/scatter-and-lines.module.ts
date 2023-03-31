d71f7b09bf84f32a2376ba83fa24c256c2eaed30
ottovenliu <otto.ven.liu@gmail.com>
2023-03-31 16:51:41 +0800

    feat: 折線圖頁面初始化 - otto_liu

--- src/app/feature/web/scatter-and-lines/scatter-and-lines.module.ts
+++ src/app/feature/web/scatter-and-lines/scatter-and-lines.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { ScatterAndLinesRoutingModule } from './scatter-and-lines-routing.module';
 import { ScatterComponent } from './scatters/scatter.component';
 import { BoxPlotComponent } from './boxplot/boxplot.component';
+import { LineComponent } from './line/line.component';
 
 @NgModule({
-  declarations: [ScatterComponent, BoxPlotComponent],
+  declarations: [ScatterComponent, BoxPlotComponent, LineComponent],
   imports: [CommonModule, SharedModule, ScatterAndLinesRoutingModule],
 })
-export class ScatterAndLinesModule {}
+export class ScatterAndLinesModule { }
