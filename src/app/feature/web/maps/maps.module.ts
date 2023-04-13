62593967507efa488f547859123924040fd58938
unknown <otto.ven.liu@gmail.com>
2023-04-13 23:56:05 +0800

    feat: tree_map_page_init - otto_liu

--- src/app/feature/web/maps/maps.module.ts
+++ src/app/feature/web/maps/maps.module.ts
@@ -1,18 +1,12 @@
 import { SharedModule } from '../../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
 import { MapsRoutingModule } from './maps-routing.module';
 import { BubbleMapComponent } from './bubble-map/bubble-map.component';
+import { TreeMapComponent } from './tree-map/tree-map.component';
 
 @NgModule({
-  declarations: [
-    BubbleMapComponent,
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    MapsRoutingModule,
-  ]
+  declarations: [BubbleMapComponent, TreeMapComponent],
+  imports: [CommonModule, SharedModule, MapsRoutingModule],
 })
-export class MapsModule { }
+export class MapsModule {}
